a7b4b79e857e6cd912fa3a2870a9a34407421ee0
fabiopak <49749142+fabiopak@users.noreply.github.com>
2019-06-01 18:10:40 +0200

    Solved minor bug

--- FPDeployer.sol
+++ FPDeployer.sol
@@ -1121,21 +1121,6 @@ interface IFPDeployer {
 }
 
 
-interface IERC20Seed {
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address owner) external view returns (uint256);
-    function transfer(address to, uint256 value) external returns (bool);
-    function approve(address spender, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-
 contract FPDeployer is CustomOwnable, IFPDeployer {
     address private fAddress;
 
